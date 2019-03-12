a8ec4a1642d457a361c6555da598f3308170b3e7
uhnut <44982339+uhnut@users.noreply.github.com>
2019-03-12 16:30:09 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,183 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+ client.on('ready', () => {
+    client.user.setGame('WIP MADE BY ROYALCANADIANNAVY AKA BATTLESHOOTER');
+    console.log('Announcements Bot#4861 has logged on!')
+  });
+
+client.on('message', msg => {
+  if (msg.content === 'd') {
+    msg.reply('TBBC IS AWESOME');
+    msg.delete('d');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Hey') {
+    msg.reply('Hello Im announcements bot I was developed and made by RoyalCanadianNavy also known as battleshooter');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '.donate') {
+    msg.reply('Thank you for donating to The Bays Business Company!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'pounds') {
+    msg.reply('Thank you for donating pounds to The Bays Business Company!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'weapons') {
+    msg.reply('Thank you for donating weapons to The Bays Business Company!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'supplies') {
+    msg.reply('Thank you for donating supplies to The Bays Business Company!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'uniforms and cosmetics') {
+    msg.reply('Thank you for donating uniforms and cosmeticsto The Bays Business Company!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'other') {
+    msg.reply('Thank you for donating something else to The Bays Business Company! Just please state what you are donating below!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'loldash') {
+    msg.reply('There will be a business event and activity test 10:30 for half hour and a bit and then we will do a combat skills assesment and more business or public patrol if bored and then on Sunday there will be lots of recruiting and public patrols if not done today please be active this is your assesment and punishments and fines will go out to in active members! :thumbsup: if you are coming this is the first part of the activity assesment');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'neee') {
+    msg.delete('neee');
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+  }
+});
+
+//THIS MUST BE THIS WAY
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+ client.on('ready', () => {
+    client.user.setGame('WIP MADE BY ROYALCANADIANNAVY AKA BATTLESHOOTER');
+    console.log('Announcements Bot#4861 has logged on!')
+  });
+
+client.on('message', msg => {
+  if (msg.content === 'd') {
+    msg.reply('TBBC IS AWESOME');
+    msg.delete('d');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Hey') {
+    msg.reply('Hello Im announcements bot I was developed and made by RoyalCanadianNavy also known as battleshooter');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '.donate') {
+    msg.reply('Thank you for donating to The Bays Business Company!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'pounds') {
+    msg.reply('Thank you for donating pounds to The Bays Business Company!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'weapons') {
+    msg.reply('Thank you for donating weapons to The Bays Business Company!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'supplies') {
+    msg.reply('Thank you for donating supplies to The Bays Business Company!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'uniforms and cosmetics') {
+    msg.reply('Thank you for donating uniforms and cosmeticsto The Bays Business Company!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'other') {
+    msg.reply('Thank you for donating something else to The Bays Business Company! Just please state what you are donating below!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'loldash') {
+    msg.reply('There will be a business event and activity test 10:30 for half hour and a bit and then we will do a combat skills assesment and more business or public patrol if bored and then on Sunday there will be lots of recruiting and public patrols if not done today please be active this is your assesment and punishments and fines will go out to in active members! :thumbsup: if you are coming this is the first part of the activity assesment');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'neee') {
+    msg.delete('neee');
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+    msg.reply('EVERYONE GET ACTIVE ACTIVITY IS IMPORTANT')
+  }
+});
+
+// THIS MUST BE THIS WAY
+client.login(process.ev.BOT_TOKEN);
